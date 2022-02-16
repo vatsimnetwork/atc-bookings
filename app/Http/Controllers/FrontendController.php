d1c88010c03510c244b28fa9d308f40afe0307e5
Matt Bozwood-Davies <12865230+mbozwood@users.noreply.github.com>
2022-02-16 21:36:33 +0000

    event callsign format
    when booking is an event, use prefix and suffix only

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -37,58 +37,15 @@ class FrontendController extends Controller
 
         foreach ($dbBookings as $dbBooking) {
             $bookings[] = [
-                'x' => $dbBooking->callsign,
+                'x' => format_callsign($dbBooking->callsign, $dbBooking->type),
                 'y' => [
                     strtotime($dbBooking->start)*1000, strtotime($dbBooking->end)*1000
                 ],
-                'fillColor' => $this->colourFromProgress(time(), strtotime($dbBooking->start), strtotime($dbBooking->end), $dbBooking->type),
+                'fillColor' => colour_from_progress(time(), strtotime($dbBooking->start), strtotime($dbBooking->end), $dbBooking->type),
             ];
         }
 
         return $bookings;
-    }
-
-    protected function colourFromProgress($now, $start, $end, $type): string
-    {
-        // future
-        if ($now < $start) {
-            if($type == 'booking') {
-                return '#8981FFCC';
-            } else if ($type == 'exam') {
-                return '#7BFF5ECC';
-            } else if ($type == 'training') {
-                return '#FF7AFFCC';
-            } else if ($type == 'event') {
-                return '#FFFF62CC';
-            } else {
-                return '#8981FFCC';
-            }
-        }
-        // past
-        if ($now > $end) {
-            if($type == 'booking') {
-                return '#0000B3CC';
-            } else if ($type == 'exam') {
-                return '#007F00CC';
-            } else if ($type == 'training') {
-                return '#A3007ACC';
-            } else if ($type == 'event') {
-                return '#A36900CC';
-            } else {
-                return '#0000B3CC';
-            }
-        }
-        if($type == 'booking') {
-            return '#3C34FFCC';
-        } else if ($type == 'exam') {
-            return '#2ECB11CC';
-        } else if ($type == 'training') {
-            return '#ED2DC6CC';
-        } else if ($type == 'event') {
-            return '#EFB515CC';
-        } else {
-            return '#3C34FFCC';
-        }
     }
 
     /**
--- app/helpers.php
+++ app/helpers.php
@@ -43,3 +43,67 @@ if (!function_exists('response_unauth')) {
         return response()->json($data, 401);
     }
 }
+
+if (!function_exists('adjust_brightness')) {
+    function adjust_brightness($hex_code, $time): string
+    {
+        $adjust_float = $time == 'current' ? 0 : ($time == 'past' ? -0.3 : 0.3);
+        $hexCode = ltrim($hex_code, '#');
+
+        if (strlen($hexCode) == 3) {
+            $hexCode = $hexCode[0] . $hexCode[0] . $hexCode[1] . $hexCode[1] . $hexCode[2] . $hexCode[2];
+        }
+
+        $hexCode = array_map('hexdec', str_split($hexCode, 2));
+
+        foreach ($hexCode as & $color) {
+            $adjustableLimit = $adjust_float < 0 ? $color : 255 - $color;
+            $adjustAmount = ceil($adjustableLimit * $adjust_float);
+
+            $color = str_pad(dechex($color + $adjustAmount), 2, '0', STR_PAD_LEFT);
+        }
+
+        return '#' . implode($hexCode);
+    }
+}
+
+if (!function_exists('colour_from_progress')) {
+    function colour_from_progress($now, $start, $end, $type): string
+    {
+        $booking = '#3C34FF';
+        $exam = '#3C34FF';
+        $training = '#ED2DC6';
+        $event = '#EFB515';
+
+        $time = $now < $start ? 'future' : ($now > $end ? 'past' : 'current');
+
+        return match($type) {
+            'exam' => adjust_brightness($exam, $time),
+            'training' => adjust_brightness($training, $time),
+            'event' => adjust_brightness($event, $time),
+            default => adjust_brightness($booking, $time),
+        };
+    }
+}
+
+if (!function_exists('format_callsign')) {
+    function format_callsign($callsign, $type = 'booking'): string
+    {
+        return match($type) {
+            'event' => format_event_callsign($callsign),
+            default => $callsign,
+        };
+    }
+}
+
+if (!function_exists('format_event_callsign')) {
+    function format_event_callsign($callsign): string
+    {
+        $count = substr_count($callsign,'_');
+        if($count > 1) {
+            $explode = explode('_', $callsign);
+            return $explode[0] . '_' . $explode[count($explode) - 1];
+        }
+        return $callsign;
+    }
+}
